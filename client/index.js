f1003a0616e9e16b448bfdbe9e96b1acfcb8a1e3
K.G. O'Hearon <ohearonkg@gmail.com>
2018-10-08 14:25:12 -0300

    interesting react router v3.

--- client/index.js
+++ client/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom";
 import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import SongList from "./components/SongList";
+import { Router, hashHistory, Route, IndexRoute } from "react-router";
+import App from "./App";
 
 const client = new ApolloClient({});
 
 const Root = () => (
   <ApolloProvider client={client}>
-    <SongList />
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={SongList} />
+      </Route>
+    </Router>
   </ApolloProvider>
 );
 
