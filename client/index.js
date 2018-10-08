4e0bede6e38da56947f62abd7707afe1c26d8c3d
K.G. O'Hearon <ohearonkg@gmail.com>
2018-10-08 13:30:08 -0300

    very basic apollo setup.

--- client/index.js
+++ client/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
 
-const Root = () => {
-  return <div>Lyrical</div>
-};
+const client = new ApolloClient({});
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
+const Root = () => (
+  <ApolloProvider client={client}>
+    <div>Lyrical</div>
+  </ApolloProvider>
 );
+
+ReactDOM.render(<Root />, document.querySelector("#root"));
